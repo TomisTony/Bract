dea46a603a70a32eaed234dbb404a28372716b3b
TomisTony <1158176767@qq.com>
2023-08-08 16:05:05 +0800

    feat: step IV

--- bract.js
+++ bract.js
@@ -20,24 +20,29 @@ function createTextElement(text) {
   };
 }
 
-function render(element, container) {
+function createDom(fiber) {
   const dom =
-    element.type === "TEXT_ELEMENT"
+    fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("") // nodeValue 会在下方和其他 props 统一注入
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
 
   const isProperty = (key) => key !== "children";
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
-      dom[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
 
-  element.props.children.forEach((child) => {
-    render(child, dom);
-  });
+  return dom;
+}
 
-  container.appendChild(dom);
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
 }
 
 let nextUnitOfWork = null;
@@ -51,8 +56,45 @@ function workLoop(deadline) {
   requestIdleCallback(workLoop); // 空闲时间执行任务
 }
 
-function performUnitOfWork(nextUnitOfWork) {
-  // TODO
+function performUnitOfWork(fiber) {
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  if(fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+  // create new fibers
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+  while(index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber, // 用于向上回溯
+      dom: null,
+    };
+    if(index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index++;
+  }
+  // return next unit of work
+  if(fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while(nextFiber) {
+    if(nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
 }
 
 const Bract = {
